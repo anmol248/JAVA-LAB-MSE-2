0a6b38ed57c9f674b61c632ccc219d810aec0613
anmol248 <anmol.25161034@kiet.edu>
2026-03-26 23:16:42 +0530

    Add files via upload

---
+++ Minmax.java
@@ -0,0 +1,19 @@
+package com.unit3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Minmax {
+
+	public static void main(String[] args) {
+		// second largest
+		
+		List<Integer> list=Arrays.asList(12,5,20,8,30,5,8);
+		System.err.println(list);
+	   Integer sortedList=list.stream().distinct().sorted((a,b)->b-a).toList().get(1);
+	  System.out.println(sortedList);
+		
+	}
+
+}
