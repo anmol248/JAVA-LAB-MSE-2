9e081eceea7468ab92bcad0102d8f16c87280cdb
anmol248 <anmol.25161034@kiet.edu>
2026-03-28 23:13:14 +0530

    Update Minmax.java

--- Minmax.java
+++ Minmax.java
@@ -11,7 +11,7 @@ public class Minmax {
 		
 		List<Integer> list=Arrays.asList(12,5,20,8,30,5,8);
 		System.err.println(list);
-	   Integer sortedList=list.stream().distinct().sorted((a,b)->b-a).toList().get(1);
+	   Integer sortedList=list.stream().distinct().sorted((a,b)->b-a).toList().get(2);
 	  System.out.println(sortedList);
 		
 	}
